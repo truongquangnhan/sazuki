49ab617662788d0b80fc2c090b82df5ba6fd187c
nhantq <nhantq@rikkeisoft.com>
2022-10-04 17:41:48 +0700

    handle API get list user

--- src/main/java/com/shizuca/sazuki/controller/UserController.java
+++ src/main/java/com/shizuca/sazuki/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/user")
@@ -15,4 +16,15 @@ public class UserController
     public ResponseEntity<User> getUser(@PathVariable Integer id){
         return ResponseEntity.ok(new User().getCloneUser(id));
     }
+
+    @GetMapping("")
+    public ResponseEntity<ArrayList<User>> getUserList(){
+        var userList = new ArrayList<User>();
+        for(var i = 0; i < 100 ; i++ ){
+            userList.add(new User().getCloneUser(i));
+        }
+
+        return ResponseEntity.ok(userList);
+    }
+
 }
